3c6592e1fc4b6fe9a9e21c23b3cab7f5249611b3
Leon <digirss@gmail.com>
2025-07-19 02:07:55 +0800

    Add express middleware for request parsing

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
 // LINE Bot configuration
